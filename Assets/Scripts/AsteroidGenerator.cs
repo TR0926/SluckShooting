5b85fe74259e327fb9335d61f65c258e8b85f7a0
Tsuchiya-Ryousuke <72895733+TR0926@users.noreply.github.com>
2020-11-24 15:35:25 +0900

    隕石を生成できるように実装

---
+++ Assets/Scripts/AsteroidGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidGenerator : MonoBehaviour
+{
+    // 隕石のPrefab用の変数
+    [SerializeField] GameObject m_asteroidPrefab = null;
+    // 生成時のタイムラグ
+    [SerializeField, Range(0.1f, 1)] float m_interval = 1f;
+    float m_timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 5秒ごとに隕石を生成する
+        m_timer += Time.deltaTime;
+
+        if(m_timer > m_interval)
+        {
+            m_timer = 0;
+            Instantiate(m_asteroidPrefab, this.gameObject.transform.position, m_asteroidPrefab.transform.rotation);
+        }
+    }
+}
